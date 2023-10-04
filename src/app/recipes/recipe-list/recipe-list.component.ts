99b25166b2ef76e4670aae7226ed2112eac012d7
joao21dev <joao21dev@gmail.com>
2023-10-04 05:37:15 -0300

    feat: listing recipes and created detail component layout'

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Teste name Recipe',
+      'Test Description',
+      'https://blog.ceraflame.com.br/wp-content/uploads/2021/06/Pizza-Napolitana-CERAFLAME-980x515.jpg'
+    ),
+    new Recipe(
+      'Teste name Recipe',
+      'Test Description',
+      'https://blog.ceraflame.com.br/wp-content/uploads/2021/06/Pizza-Napolitana-CERAFLAME-980x515.jpg'
+    ),
+    new Recipe(
+      'Teste name Recipe',
+      'Test Description',
+      'https://blog.ceraflame.com.br/wp-content/uploads/2021/06/Pizza-Napolitana-CERAFLAME-980x515.jpg'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
